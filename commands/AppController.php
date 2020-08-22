e06cc2a9bfa1d677c4e6c967379b624b850f3278
dailcoyote <dailcoyote@gmail.com>
2020-08-23 05:18:34 +0600

    App command: create author

---
+++ commands/AppController.php
@@ -0,0 +1,38 @@
+<?php
+/**
+ * @link http://www.yiiframework.com/
+ * @copyright Copyright (c) 2008 Yii Software LLC
+ * @license http://www.yiiframework.com/license/
+ */
+
+namespace app\commands;
+
+use app\models\Author;
+use yii\console\Controller;
+use yii\helpers\Console;
+
+/**
+ * This command echoes the first argument that you have entered.
+ *
+ * This command is provided as an example for you to learn how to create console commands.
+ *
+ * @author Qiang Xue <qiang.xue@gmail.com>
+ * @since 2.0
+ */
+class AppController extends Controller
+{
+    public function actionCreateAuthor($firstname, $lastname, $occupation = "writer")
+    {
+        $author = new Author();
+        $author->firstname = $firstname;
+        $author->lastname = $lastname;
+        $author->occupation = $occupation;
+
+        if($author->save()) {
+            Console::output("Author saved");
+        } else {
+            var_dump($author->errors);
+            Console::output("Author not saved");
+        }
+    }
+}
